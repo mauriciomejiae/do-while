1a768dcb39c223701664d2d56cd5385ad0de9845
Mauricio Mejia Estevez <mme0504@gmail.com>
2018-08-07 13:57:10 -0500

    Add files via upload

---
+++ do while/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            do
+            {
+                Console.WriteLine("Esta es la vez numero " + contador + " que pasa por aqui");
+                contador++;
+            } while (contador < 10);
+            Console.ReadKey();
+
+        }
+    }
+}
